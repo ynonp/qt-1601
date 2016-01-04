c49758565abd6467504491f896434b9029d844de
ynon <ynon@debian>
2016-01-04 14:57:41 -0400

    more custom widgets

---
+++ 06-custom-widgets/tictactoeboard.cpp
@@ -0,0 +1,6 @@
+#include "tictactoeboard.h"
+
+TicTacToeBoard::TicTacToeBoard(QWidget *parent) :
+    QWidget(parent)
+{
+}
---
+++ 06-custom-widgets/tictactoeboard.h
@@ -0,0 +1,18 @@
+#ifndef TICTACTOEBOARD_H
+#define TICTACTOEBOARD_H
+
+#include <QWidget>
+
+class TicTacToeBoard : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TicTacToeBoard(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // TICTACTOEBOARD_H
